0567bf91563adc6c15004b5ee793e5f6bb70e59e
皮卡丘打西瓜汁 <s0610078@my.cmsh.cyc.edu.tw>
2020-06-02 13:57:55 +0800

    Add files via upload

---
+++ 097-我不說髒話 (續).cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(int argc, char** argv)
+{
+	int n;
+	cin >> n;
+	for (int i = 1; i <= n; i++)
+		cout << i << ". I don't say swear words!" << endl;
+	system("PAUSE");
+	return 0;
+}
