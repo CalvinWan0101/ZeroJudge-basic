e07b435a30f75463d11c4f80a2abd86ec5b6eb3c
皮卡丘打西瓜汁 <s0610078@my.cmsh.cyc.edu.tw>
2020-06-02 13:56:21 +0800

    Update and rename 097-我不說髒話 (續)(尚未解決).cpp to 097-我不說髒話 (續).cpp

--- 097-我不說髒話 (續).cpp
+++ 097-我不說髒話 (續).cpp
@@ -2,6 +2,10 @@
 using namespace std;
 int main(int argc, char** argv)
 {
+	int n;
+	cin >> n;
+	for (int i = 1; i <= n; i++)
+		cout << i << ". I don't say swear words!" << endl;
 	system("PAUSE");
 	return 0;
 }
