a8828d0c815c2d569d2f57a36af52e40cc815882
皮卡丘打西瓜汁 <s0610078@my.cmsh.cyc.edu.tw>
2020-06-05 15:02:34 +0800

    Rename 091-stack 堆疊的模板題.cpp to b923-stack 堆疊的模板題.cpp

--- b923-stack 堆疊的模板題.cpp
+++ b923-stack 堆疊的模板題.cpp
@@ -3,11 +3,11 @@ using namespace std;
 int main(int argc, char** argv)
 {
 	int n;
-	cout << "¾Ş§@¶q¡G";
+	cout << "æ“ä½œé‡ï¼š";
 	cin >> n;
 	int input;
 	int array[100] = {0};
-	//¦s¨ú©R¥O¦C
+	//å­˜å–å‘½ä»¤åˆ—
 	int command[100][2] = {0};
 	for (int i = 0; i < n; i++)
 	{
@@ -15,7 +15,7 @@ int main(int argc, char** argv)
 		if (command[i][0] == 3)
 			cin >> command[i][1];
 	}
-	cout << "¹wÄı©R¥O¦C" << endl;
+	cout << "é è¦½å‘½ä»¤åˆ—" << endl;
 	for (int i = 0; i < n; i++)
 		cout << command[i][0]<<" "<<command[i][1]<<endl;
 	cout << endl;
@@ -24,7 +24,7 @@ int main(int argc, char** argv)
 	{
 		switch (command[i][0])
 		{
-			//§R°£
+			//åˆªé™¤
 		case 1:
 			for (int i = 0; i < 100; i++)
 				if (array[i] != 0)
@@ -33,7 +33,7 @@ int main(int argc, char** argv)
 					break;
 				}
 			break;
-			//¿é¥X³»ºİ¤¸¯À
+			//è¼¸å‡ºé ‚ç«¯å…ƒç´ 
 		case 2:
 			for (int i = 0; i < 100; i++)
 				if (array[i] != 0)
@@ -42,7 +42,7 @@ int main(int argc, char** argv)
 					break;
 				}
 			break;
-			//Åª¤J¤@­Ó¾ã¼Æ¶i¤J°ïÅ|
+			//è®€å…¥ä¸€å€‹æ•´æ•¸é€²å…¥å †ç–Š
 		case 3:
 			for (int j = 99; j >= 0; j--)
 				if (array[j] == 0)
