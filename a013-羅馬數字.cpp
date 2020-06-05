d349db29f4f0254a749817f836c4c2cce7aee209
皮卡丘打西瓜汁 <s0610078@my.cmsh.cyc.edu.tw>
2020-06-05 11:29:03 +0800

    Rename 009-羅馬數字.cpp to a013-羅馬數字.cpp

--- a013-羅馬數字.cpp
+++ a013-羅馬數字.cpp
@@ -3,26 +3,26 @@
 using namespace std;
 int main(int argc, char** argv)
 {
-	//���D���
-	//ù���Ʀr�@�զr���N���@��Ʀr
-	//�@�դS�����p�iV�j�@�Ӧr�����@�եH�Φp�iIV�j��Ӧr�����@��
-	//���]�ߥi�H��Ӧr�����@�ժ��}�Y�r���p�iI�j�iX�j�iC�j
-	//�A�P�_�u���W�@�Ӭ��@�ժ��p�iV�j�iL�j�iD�j�iM�j
+	//解題思路
+	//羅馬數字一組字母代表一位數字
+	//一組又有分如【V】一個字母為一組以及如【IV】兩個字母為一組
+	//先設立可以兩個字母為一組的開頭字母如【I】【X】【C】
+	//再判斷只能單獨一個為一組的如【V】【L】【D】【M】
 	string a, b;
-	cout << "�Ĥ@��ù���Ʀr�G";
+	cout << "第一個羅馬數字：";
 	cin >> a;
-	cout << "�ĤG��ù���Ʀr�G";
+	cout << "第二個羅馬數字：";
 	cin >> b;
-	//�ഫa���Ʀr
+	//轉換a為數字
 	int suma = 0;
 	string buffer[2];
-	//�ഫstring a���Ʀr
+	//轉換string a為數字
 	for (int i = 0; i < a.length(); i++)
 	{
 		buffer[0] = a[i];
 		buffer[1] = a[i + 1];
 
-		//I�}�Y�A�]�t�iI�j�iIV�j�iIX�j
+		//I開頭，包含【I】【IV】【IX】
 		if (buffer[0].compare("I") == 0)
 		{
 			if (buffer[1].compare("V") == 0)
@@ -38,7 +38,7 @@ int main(int argc, char** argv)
 			else
 				suma += 1;
 		}
-		//X�}�Y�A�]�t�iX�j�iXL�j�iXC�j
+		//X開頭，包含【X】【XL】【XC】
 		if (buffer[0].compare("X") == 0)
 		{
 			if (buffer[1].compare("L") == 0)
@@ -54,7 +54,7 @@ int main(int argc, char** argv)
 			else
 				suma += 10;
 		}
-		//C�}�Y�A�]�t�iC�j�iCD�j�iCM�j
+		//C開頭，包含【C】【CD】【CM】
 		if (buffer[0].compare("C") == 0)
 		{
 			if (buffer[1].compare("D") == 0)
@@ -70,28 +70,28 @@ int main(int argc, char** argv)
 			else
 				suma += 100;
 		}
-		//��WV
+		//單獨V
 		if (buffer[0].compare("V") == 0)
 			suma += 5;
-		//��WL
+		//單獨L
 		if (buffer[0].compare("L") == 0)
 			suma += 50;
-		//��WD
+		//單獨D
 		if (buffer[0].compare("D") == 0)
 			suma += 500;
-		//��WM
+		//單獨M
 		if (buffer[0].compare("M") == 0)
 			suma += 1000;
 	}
 	cout << a << "=" << suma << endl;
-	//�ഫstring b���Ʀr
+	//轉換string b為數字
 	int sumb = 0;
 	for (int i = 0; i < b.length(); i++)
 	{
 		buffer[0] = b[i];
 		buffer[1] = b[i + 1];
 
-		//I�}�Y�A�]�t�iI�j�iIV�j�iIX�j
+		//I開頭，包含【I】【IV】【IX】
 		if (buffer[0].compare("I") == 0)
 		{
 			if (buffer[1].compare("V") == 0)
@@ -107,7 +107,7 @@ int main(int argc, char** argv)
 			else
 				sumb += 1;
 		}
-		//X�}�Y�A�]�t�iX�j�iXL�j�iXC�j
+		//X開頭，包含【X】【XL】【XC】
 		if (buffer[0].compare("X") == 0)
 		{
 			if (buffer[1].compare("L") == 0)
@@ -123,7 +123,7 @@ int main(int argc, char** argv)
 			else
 				sumb += 10;
 		}
-		//C�}�Y�A�]�t�iC�j�iCD�j�iCM�j
+		//C開頭，包含【C】【CD】【CM】
 		if (buffer[0].compare("C") == 0)
 		{
 			if (buffer[1].compare("D") == 0)
@@ -139,29 +139,29 @@ int main(int argc, char** argv)
 			else
 				sumb += 100;
 		}
-		//��WV
+		//單獨V
 		if (buffer[0].compare("V") == 0)
 			sumb += 5;
-		//��WL
+		//單獨L
 		if (buffer[0].compare("L") == 0)
 			sumb += 50;
-		//��WD
+		//單獨D
 		if (buffer[0].compare("D") == 0)
 			sumb += 500;
-		//��WM
+		//單獨M
 		if (buffer[0].compare("M") == 0)
 			sumb += 1000;
 	}
 	cout << b << "=" << sumb << endl;
 
 	int sum = suma - sumb;
-	//�Nsum�Hù���Ʀr�����A��X
+	//將sum以羅馬數字的型態輸出
 
-	//�ഫ���
-	//�N�Ҧ�ù���Ʀr���զX�C�X
-	//�N���ഫ���Ʀr�q�j���p�A�s�^
-	//�P�ɼW�[�Ҵ�Ʀr�ҹ�����ù���Ʀr
-	//���쵲�G��0
+	//轉換思路
+	//將所有羅馬數字的組合列出
+	//將欲轉換的數字從大減到小再存回
+	//同時增加所減掉數字所對應的羅馬數字
+	//直到結果為0
 	string rom[13] = { "M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I" };
 	int romnum[13] = { 1000,900,500,400,100,90,50,40,10,9,5,4,1 }; \
 		string print = "";
