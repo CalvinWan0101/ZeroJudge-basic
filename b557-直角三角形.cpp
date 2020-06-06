04df5aa1e4e689deef500d45ff66afd4c223f12a
皮卡丘打西瓜汁 <s0610078@my.cmsh.cyc.edu.tw>
2020-06-06 18:41:14 +0800

    Update b557-直角三角形.cpp

--- b557-直角三角形.cpp
+++ b557-直角三角形.cpp
@@ -3,27 +3,38 @@ using namespace std;
 int main(int argc, char** argv)
 {
 	int n;
-	cout << "木棒數量：";
-	cin >> n;
 	int array[100];
-	for (int i = 0; i < n; i++)
-	{
-		cout << "第" << i + 1 << "根木棒長度：";
-		cin >> array[i];
-	}
 	int count = 0;
-	for (int i = 0; i < n; i++)
-		for (int j = 0; j < n; j++)
-			for (int k = 0; k < n; k++)
+	int m;
+	cin >> m;
+	for (int i = 0; i < m; i++)
+	{
+		count = 0;
+		cout << "木棒數量：";
+		cin >> n;
+		for (int i = 0; i < n; i++)
+		{
+			cout << "第" << i + 1 << "根木棒長度：";
+			cin >> array[i];
+		}
+		for (int i = 0; i < n; i++)
+		{
+			for (int j = 0; j < n; j++)
 			{
-				if (i != j && i != k && j != k)
-					if (pow(array[i], 2) + pow(array[j], 2) == pow(array[k], 2) ||
-						pow(array[i], 2) + pow(array[k], 2) == pow(array[j], 2) ||
-						pow(array[j], 2) + pow(array[k], 2) == pow(array[i], 2))
-						if (array[i] > array[j] && array[j] > array[k])
-							count++;
+				if (i == j)
+					continue;
+				for (int k = 0; k < n; k++)
+				{
+					if (i == k || j == k)
+						continue;
+					if (pow(array[i], 2) + pow(array[j], 2) == pow(array[k], 2) || pow(array[i], 2) + pow(array[k], 2) == pow(array[j], 2) || pow(array[j], 2) + pow(array[k], 2) == pow(array[i], 2))
+						if(array[i] <= array[j]&&array[j]<=array[k])
+						count++;
+				}
 			}
-	cout << "可以構成" << count << "種直角三角形" << endl;
+		}
+		cout << "可以構成" << count << "種直角三角形" << endl;
+	}
 	system("PAUSE");
 	return 0;
 }
