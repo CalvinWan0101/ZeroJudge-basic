a03bcc54f24fa1ff4cb1fbdccd6ce9ed63b005ff
皮卡丘打西瓜汁 <s0610078@my.cmsh.cyc.edu.tw>
2020-06-08 08:15:10 +0800

    Update a981-求和問題(有待改進).cpp

--- a981-求和問題(有待改進).cpp
+++ a981-求和問題(有待改進).cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <iomanip>
 using namespace std;
-//使用dfs進行排列(dfs的bucket需要再思考)
+//使用dfs進行排列
 int answer[31];
 //記錄array每一項使用狀況
 int bucket[31] = { 0 };
@@ -30,25 +30,38 @@ void dfs(int step, int end, int n, int m, int array[31])
 			for (int j = i + 1; j <= end; j++)
 				if (answer[i] > answer[j])
 					know = 1;
-		if (cou == m && know == 0)
-		{
-			for (int i = 1; i <= end; i++)
-				cout << setw(3) << array[answer[i]];
-			cout << endl;
-		}
+		if (cou == m)
+			//if (know == 0)
+			{
+				for (int i = 1; i <= end; i++)
+					cout << setw(3) << array[answer[i]];
+				cout << endl;
+			}
 		return;
 	}
 	//一般狀況
-	for (int i = 1; i <= n; i++)
-	{
-		if (bucket[i] == 0)
+	if (step == 1)
+		for (int i = 1; i <= n; i++)
 		{
-			answer[step] = i;
-			bucket[i] = 1;
-			dfs(step + 1, end, n, m, array);
-			bucket[i] = 0;
+			if (bucket[i] == 0)
+			{
+				answer[step] = i;
+				bucket[i] = 1;
+				dfs(step + 1, end, n, m, array);
+				bucket[i] = 0;
+			}
 		}
-	}
+	else
+		for (int i = answer[step - 1] + 1; i <= n; i++)
+		{
+			if (bucket[i] == 0)
+			{
+				answer[step] = i;
+				bucket[i] = 1;
+				dfs(step + 1, end, n, m, array);
+				bucket[i] = 0;
+			}
+		}
 }
 int main(int argc, char** argv)
 {
@@ -59,10 +72,10 @@ int main(int argc, char** argv)
 	//輸入母陣列(從1開始輸入)
 	for (int i = 1; i <= n; i++)
 		cin >> array[i];
-	cout << "排序前：";
+	/*cout << "排序前：";
 	for (int i = 1; i <= n; i++)
-		cout << setw(3) << array[i];
-	//先整理一下母陣列(由小到大排序)
+		cout << setw(3) << array[i];*/
+		//先整理一下母陣列(由小到大排序)
 	int buffer;
 	for (int i = 1; i <= n - 1; i++)
 		for (int j = i + 1; j <= n; j++)
@@ -72,11 +85,11 @@ int main(int argc, char** argv)
 				array[j] = array[i];
 				array[i] = buffer;
 			}
-	cout << endl;
+	/*cout << endl;
 	cout << "排序後：";
 	for (int i = 1; i <= n; i++)
 		cout << setw(3) << array[i];
-	cout << endl;
+	cout << endl;*/
 	//假設n=10
 	//就要嘗試在母陣列中取1,2,3,4,5,6......8,9,10個數字組合
 	for (int i = 10; i >= 1; i--)
