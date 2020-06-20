8c3f437617a3dc767dd233c7afb8090eb8fa0bc0
協同意大利麵的最後希望 <s0610078@my.cmsh.cyc.edu.tw>
2020-06-20 09:30:32 +0800

    Update b860-獨角蟲進化計算器.cpp

--- b860-獨角蟲進化計算器.cpp
+++ b860-獨角蟲進化計算器.cpp
@@ -1,49 +1,24 @@
 #include <iostream>
 using namespace std;
-int main(int argc, char** argv)
+int main()
 {
-	int n;
-	cout << "糖果數：";
-	cin >> n;
-	int before;
-	cout << "獨角仙數：";
-	cin >> before;
-	//進化後的數量
-	int after=0;
-	int count = 0;
-	while (before + after + n >= 12)
+	int a, b, c = 0, d = 0;
+	while (cin >> a >> b)
 	{
-		if (n >= 12)
+		while (a && b)
 		{
-			n -= 12;
-			before--;
-			after++;
-			count++;
-			n++;
+			if (a >= 12)
+			{
+				a -= 11;
+				c++;
+			}
+			else
+			{
+				a++;
+				b--;
+			}
 		}
-		else if (n < 12 && after + n >= 12)
-		{
-			n -= n;
-			after -= (12 - n);
-			before--;
-			after++;
-			count++;
-			n++;
-		}
-		else if (n < 12 && after + n < 12 && before + after + n >= 13)
-		{
-			n -= n;
-			after -= after;
-			before--;
-			before -= (12 - n - after);
-			after++;
-			count++;
-			n++;
-		}
-		else
-			break;
+		cout << c << endl;
 	}
-	cout << count << endl;
-	system("PAUSE");
 	return 0;
 }
