23ed5a454de9ab353dc8644f0e7dd54de5a59ab6
協同意大利麵的最後希望 <s0610078@my.cmsh.cyc.edu.tw>
2020-06-21 14:40:48 +0800

    Update b923-stack 堆疊的模板題.cpp

--- b923-stack 堆疊的模板題.cpp
+++ b923-stack 堆疊的模板題.cpp
@@ -5,41 +5,29 @@ int main()
 	//1-刪除頂端元素
 	//2-輸出頂端元素
 	//3-丟數字進堆疊
-	int n, a, array[100001] = { -1 }, b;
-	while (cin >> n)
+	int n;
+	cin >> n;
+	int a,b;
+	int array[10000] = {0};
+	int top = 0;
+	for (int i = 1; i <= n; i++)
 	{
-		for (int i = 0; i < n; i++)
+		cin >> a;
+		switch (a)
 		{
-			cin >> a;
-			if (a == 1)
-			{
-				for (int i = 0; i < 100001; i++)
-					if (array[i] == -1)
-					{
-						array[i - 1] = -1;
-						break;
-					}
-			}
-			else if (a == 2)
-			{
-				for (int i = 0; i < 100001; i++)
-					if (array[i] == -1)
-					{
-						cout << array[i - 1] << endl;
-						break;
-					}
-			}
-			else if (a == 3)
-			{
-				cin >> b;
-				for (int i = 0; i < 100001; i++)
-					if (array[i] == -1)
-					{
-						array[i] = b;
-						break;
-					}
-			}
+		case 1:
+			top--;
+			array[top] = 0;
+			break;
+		case 2:
+			cout << array[top-1] << endl;
+			break;
+		case 3:
+			cin >> b;
+			array[top] = b;
+			top++;
+			break;
 		}
 	}
-	return 0;
+		return 0;
 }
