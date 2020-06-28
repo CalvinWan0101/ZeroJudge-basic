1949a537482b8fcb352602b5c31fcf35b13c05d2
協同意大利麵的最後希望 <s0610078@my.cmsh.cyc.edu.tw>
2020-06-28 14:52:23 +0800

    Update and rename a104-排序.cpp to a104-排序/ a104-排序.cpp

---
+++ a104-排序/ a104-排序.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int main(int argc, char** argv)
+{
+	int a;
+	int array[1000] = { 0 };
+	while (cin >> a)
+	{
+		for (int i = 0; i < a; i++)
+			cin >> array[i];
+		int buffer;
+		for (int i = 0; i < a - 1; i++)
+			for (int j = i; j < a; j++)
+				if (array[i] > array[j])
+				{
+					buffer = array[j];
+					array[j] = array[i];
+					array[i] = buffer;
+				}
+		for (int i = 0; i < a; i++)
+			if (i == a - 1)
+				cout << array[i] << endl;
+			else
+				cout << array[i] << " ";
+	}
+	return 0;
+}
