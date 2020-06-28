5dc02dcd91c86a52b94b31ee6c68c5a404ad3704
協同意大利麵的最後希望 <s0610078@my.cmsh.cyc.edu.tw>
2020-06-28 13:55:37 +0800

    Update and rename a053-Sagit's 計分程式.cpp to a053-Sagit's 計分程式/a053-Sagit's 計分程式.cpp

---
+++ a053-Sagit's 計分程式/a053-Sagit's 計分程式.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int in, score;
+	while (cin >> in)
+	{
+		if (in <= 10)
+			score = in * 6;
+		else if (in <= 20)
+			score = 60 + (in - 10) * 2;
+		else if (in <= 40)
+			score = 60 + 20 + (in - 20) * 1;
+		else
+			score = 100;
+		cout << score << endl;
+	}
+	return 0;
+}
