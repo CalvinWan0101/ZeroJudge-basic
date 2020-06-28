53ea5e237dbf902b066a7cd99fa995519544c77e
協同意大利麵的最後希望 <s0610078@my.cmsh.cyc.edu.tw>
2020-06-28 15:39:20 +0800

    Update a147-Print it all.cpp

--- a147-Print it all/a147-Print it all.cpp
+++ a147-Print it all/a147-Print it all.cpp
@@ -9,15 +9,8 @@ int main()
 		if (n == 0)
 			break;
 		for (int i = 1; i < n; i++)
-		{
 			if (i % 7 != 0)
-			{
-				if (i == 1)
-					cout << i;
-				else
-					cout << " " << i;
-			}
-		}
+				cout << i << " ";
 		cout << endl;
 	}
 	return 0;
