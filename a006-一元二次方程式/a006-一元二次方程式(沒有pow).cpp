0cc7a7d0c53ff509c287079e533d005b07f74b5f
協同意大利麵的最後希望 <s0610078@my.cmsh.cyc.edu.tw>
2020-06-29 11:18:08 +0800

    Create  a006-一元二次方程式(沒有pow).cpp

---
+++ a006-一元二次方程式/a006-一元二次方程式(沒有pow).cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main(int argc, char** argv)
+{
+	int a, b, c;
+	int  x1, x2, x3;
+	cin >> a >> b >> c;
+	if (b * b - 4 * a * c > 0)
+	{
+		//Two different roots x1=2 , x2=-5
+		x1 = (-b + sqrt(b * b - 4 * a * c)) / (2 * a);
+		x2 = (-b - sqrt(b * b - 4 * a * c)) / (2 * a);
+		if (x1 > x2)
+			cout << "Two different roots x1=" << x1 << " , x2=" << x2 << endl;
+		else if (x1 < x2)
+			cout << "Two different roots x1=" << x2 << " , x2=" << x1 << endl;
+	}
+	else if (b * b - 4 * a * c == 0)
+	{
+		x3 = (-b + sqrt(b * b - 4 * a * c)) / (2 * a);
+		cout << "Two same roots x=" << x3;
+	}
+	else
+		cout << "No real root";
+	return 0;
+}
