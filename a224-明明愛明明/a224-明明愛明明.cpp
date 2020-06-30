3cfbb2e5f8e5f4ef6c7e74d521d61b8ce76ddf55
協同意大利麵的最後希望 <s0610078@my.cmsh.cyc.edu.tw>
2020-06-30 10:05:33 +0800

    Update a224-明明愛明明.cpp

--- a224-明明愛明明/a224-明明愛明明.cpp
+++ a224-明明愛明明/a224-明明愛明明.cpp
@@ -3,133 +3,26 @@
 using namespace std;
 int main(int argc, char** argv)
 {
-	//目的：判斷所輸入的字母是否能組成迴文
-	int array[27] = { 0 };
+	int bucket[27] = { 0 };
 	string input;
-	cout << "欲檢驗的字串：";
-	cin >> input;
-	for (int i = 0; i < input.length(); i++)
+	while (cin >> input)
 	{
-		//將各字母出現的次數存入預先設立的整數陣列當中
-		switch (input[i])
-		{
-		case 'a':
-		case 'A':
-			array[1]++;
-			break;
-		case 'b':
-		case 'B':
-			array[2]++;
-			break;
-		case 'c':
-		case 'C':
-			array[3]++;
-			break;
-		case 'd':
-		case 'D':
-			array[4]++;
-			break;
-		case 'e':
-		case 'E':
-			array[5]++;
-			break;
-		case 'f':
-		case 'F':
-			array[6]++;
-			break;
-		case 'g':
-		case 'G':
-			array[7]++;
-			break;
-		case 'h':
-		case 'H':
-			array[8]++;
-			break;
-		case 'i':
-		case 'I':
-			array[9]++;
-			break;
-		case 'j':
-		case 'J':
-			array[10]++;
-			break;
-		case 'k':
-		case 'K':
-			array[11]++;
-			break;
-		case 'l':
-		case 'L':
-			array[12]++;
-			break;
-		case 'm':
-		case 'M':
-			array[13]++;
-			break;
-		case 'n':
-		case 'N':
-			array[14]++;
-			break;
-		case 'o':
-		case 'O':
-			array[15]++;
-			break;
-		case 'p':
-		case 'P':
-			array[16]++;
-			break;
-		case 'q':
-		case 'Q':
-			array[17]++;
-			break;
-		case 'r':
-		case 'R':
-			array[18]++;
-			break;
-		case 's':
-		case 'S':
-			array[19]++;
-			break;
-		case 't':
-		case 'T':
-			array[20]++;
-			break;
-		case 'u':
-		case 'U':
-			array[21]++;
-			break;
-		case 'v':
-		case 'V':
-			array[22]++;
-			break;
-		case 'w':
-		case 'W':
-			array[23]++;
-			break;
-		case 'x':
-		case 'X':
-			array[24]++;
-			break;
-		case 'y':
-		case 'Y':
-			array[25]++;
-			break;
-		case 'z':
-		case 'Z':
-			array[26]++;
-			break;
-		}
+		for (int i = 0; i < 27; i++)
+			bucket[i] = 0;
+		for (int i = 0; i < input.length(); i++)
+			if (input[i] >= 65 && input[i] <= 90 || input[i] >= 97 && input[i] <= 122)
+				if (input[i] >= 65 && input[i] <= 90)
+					bucket[input[i] - 64]++;
+				else if (input[i] >= 97 && input[i] <= 122)
+					bucket[input[i] - 96]++;
+		int count = 0;
+		for (int i = 1; i <= 26; i++)
+			if (bucket[i] % 2 != 0)
+				count++;
+		if (count >= 2)
+			cout << "no..." << endl;
+		else
+			cout << "yes !" << endl;
 	}
-	//存取出現次數為基數的次數
-	int count = 0;
-	for (int i = 1; i <= 26; i++)
-	{
-		if (array[i] % 2 != 0)
-			count++;
-	}
-	if (count >= 2)
-		cout << "no..." << endl;
-	else
-		cout << "yes!!!" << endl;
-	system("PAUSE");
 	return 0;
 }
