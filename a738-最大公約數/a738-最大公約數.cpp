7072b00a8c59e28fe5b7594b55e7b8f5bca8ceb7
協同意大利麵的最後希望 <s0610078@my.cmsh.cyc.edu.tw>
2020-06-30 15:07:21 +0800

    Update and rename a738-最大公約數.cpp to a738-最大公約數/a738-最大公約數.cpp

---
+++ a738-最大公約數/a738-最大公約數.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+int gcd(int a, int b)
+{
+	return a % b == 0 ? b : gcd(b, a % b);
+}
+int main()
+{
+	int a, b;
+	while (cin >> a >> b)
+		cout << gcd(a, b) << endl;
+	return 0;
+}
