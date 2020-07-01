3b89679ce0e7a4b205466c6104b5cb153345988d
協同意大利麵的最後希望 <s0610078@my.cmsh.cyc.edu.tw>
2020-07-01 09:02:21 +0800

    Update and rename b511-換銅板.cpp to b511-換銅板/b511-換銅板.cpp

--- b511-換銅板/b511-換銅板.cpp
+++ b511-換銅板/b511-換銅板.cpp
@@ -1,11 +1,10 @@
 #include <iostream>
-#include <iomanip>
 using namespace std;
 int arr[4] = { 0 };
 int sum;
 void cat(int a, int n, int orgin, int kind[4])
 {
-	if (a == n-1)
+	if (a == n - 1)
 	{
 		for (arr[a] = 0; arr[a] <= orgin; arr[a]++)
 		{
@@ -20,9 +19,9 @@ void cat(int a, int n, int orgin, int kind[4])
 				for (int i = 0; i < n; i++)
 				{
 					if (i != n - 1)
-						cout << setw(2) << arr[i] << ",";
+						cout << arr[i] << ",";
 					else
-						cout << setw(2) << arr[i] << ")" << endl;
+						cout << arr[i] << ")" << endl;
 				}
 			}
 		}
@@ -31,23 +30,16 @@ void cat(int a, int n, int orgin, int kind[4])
 	for (arr[a] = 0; arr[a] <= orgin; arr[a]++)
 		cat(a + 1, n, orgin, kind);
 }
-int main(int argc, char** argv)
+int main()
 {
 	int n;
-	cout << "銅板種類：";
 	cin >> n;
 	//種類概念類似倍率
 	int kind[4];
-	for (int i =0 ; i < n; i++)
-	{
-		cout << "第" << i + 1 << "種面額：";
+	for (int i = 0; i < n; i++)
 		cin >> kind[i];
-	}
 	int orgin;
-	cout << "所給金額：";
 	cin >> orgin;
-	//回溯法
 	cat(0, n, orgin, kind);
-	system("PAUSE");
 	return 0;
 }
