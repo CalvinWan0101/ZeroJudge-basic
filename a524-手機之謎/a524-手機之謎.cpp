7f38398f538081237cb0a47f6e7e4c7ba947ccde
協同意大利麵的最後希望 <s0610078@my.cmsh.cyc.edu.tw>
2020-07-01 08:56:09 +0800

    Update a524-手機之謎.cpp

--- a524-手機之謎/a524-手機之謎.cpp
+++ a524-手機之謎/a524-手機之謎.cpp
@@ -1,17 +1,14 @@
 #include<iostream>
 using namespace std;
 int n, answer[10], bucket[10] = { 0 };
-void dfs(int step)
-{
-	if (step == n + 1)
-	{
+void dfs(int step) {
+	if (step == n + 1) {
 		for (int i = 1; i <= n; i++)
 			cout << answer[i];
-		cout << endl;
+		cout << '\n';
 	}
 	for (int i = n; i >= 1; i--)
-		if (bucket[i] == 0)
-		{
+		if (bucket[i] == 0) {
 			answer[step] = i;
 			bucket[i] = 1;
 			dfs(step + 1);
@@ -22,5 +19,4 @@ int main()
 {
 	while (cin >> n)
 		dfs(1);
-	return 0;
 }
