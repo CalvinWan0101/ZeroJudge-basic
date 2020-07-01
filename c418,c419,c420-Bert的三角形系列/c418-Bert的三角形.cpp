b82051b66ede7b937739109ca38ab15b28a29d46
協同意大利麵的最後希望 <s0610078@my.cmsh.cyc.edu.tw>
2020-07-01 09:20:24 +0800

    Update and rename c418-Bert的三角形 (1).cpp to c418,c419,c420-Bert的三角形系列/c418-Bert的三角形.cpp

--- c418,c419,c420-Bert的三角形系列/c418-Bert的三角形.cpp
+++ c418,c419,c420-Bert的三角形系列/c418-Bert的三角形.cpp
@@ -1,17 +1,14 @@
 #include <iostream>
 using namespace std;
-int main(int argc, char** argv)
+int main()
 {
 	int n;
 	while (cin >> n)
-	{
 		for (int i = 1; i <= n; i++)
 		{
 			for (int j = 1; j <= i; j++)
 				cout << "*";
-			cout << endl;
+			cout << '\n';
 		}
-	}
-	system("PAUSE");
 	return 0;
 }
