0002dc2cd3c42de4885bdf098171998a50c5ee47
協同意大利麵的最後希望 <s0610078@my.cmsh.cyc.edu.tw>
2020-07-25 20:22:54 +0800

    Update a002-簡易加法.cpp

--- a002-簡易加法/a002-簡易加法.cpp
+++ a002-簡易加法/a002-簡易加法.cpp
@@ -1,9 +1,8 @@
 #include <iostream>
 using namespace std;
-int main(int argc, char** argv)
+int main()
 {
-	int a, b;
-	while(cin>>a>>b)
-		cout<<a+b<<endl;
-	return 0;
+	string a;
+	while(cin >> a)
+	cout << "hello, "<< a << '\n';
 }
