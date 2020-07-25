0d0af97507d4a795f4b25be059574ad8930c84db
協同意大利麵的最後希望 <s0610078@my.cmsh.cyc.edu.tw>
2020-07-25 20:39:55 +0800

    Update and rename a006-一元二次方程式.cpp to a006-一元二次方程式(使用pow).cpp

--- a006-一元二次方程式/a006-一元二次方程式(使用pow).cpp
+++ a006-一元二次方程式/a006-一元二次方程式(使用pow).cpp
@@ -12,9 +12,9 @@ int main(int argc, char** argv)
 		x1 = (-b + sqrt(b * b - 4 * a * c)) / (2 * a);
 		x2 = (-b - sqrt(b * b - 4 * a * c)) / (2 * a);
 		if (x1 > x2)
-			cout << "Two different roots x1=" << x1 << " , x2=" << x2 << endl;
+			cout << "Two different roots x1=" << x1 << " , x2=" << x2 << '\n';
 		else if (x1 < x2)
-			cout << "Two different roots x1=" << x2 << " , x2=" << x1 << endl;
+			cout << "Two different roots x1=" << x2 << " , x2=" << x1 << '\n';
 	}
 	else if ((pow(b, 2) - 4 * a * c) == 0)
 	{
