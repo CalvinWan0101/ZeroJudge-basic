760f595b7cf7be1566812dbab98006ecbf3a0194
協同意大利麵的最後希望 <s0610078@my.cmsh.cyc.edu.tw>
2020-07-27 18:56:55 +0800

    Update a010-因數分解.cpp

--- a010-因數分解/a010-因數分解.cpp
+++ a010-因數分解/a010-因數分解.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
-#include<iomanip>
 using namespace std;
-int main(int argc, char** argv)
+int main()
 {
 	int n;
 	cin >> n;
@@ -18,5 +17,4 @@ int main(int argc, char** argv)
 		else if (count > 1)
 			cout << i << "^" << count << ((n == 1) ? "\n" : " * ");
 	}
-	return 0;
 }
