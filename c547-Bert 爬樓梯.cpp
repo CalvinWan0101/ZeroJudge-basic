5628770dadad2ee72e48e07ec33d0b4d1496e0d2
協同意大利麵的最後希望 <s0610078@my.cmsh.cyc.edu.tw>
2021-02-09 23:48:37 +0800

    Update c547-Bert 爬樓梯.cpp

--- c547-Bert 爬樓梯.cpp
+++ c547-Bert 爬樓梯.cpp
@@ -2,7 +2,9 @@
 using namespace std;
 int main()
 {
-	int array[10001];
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+   int array[10001];
 	array[0] = 1;
 	array[1] = 1;
 	for (int i = 2; i < 10000; i++)
