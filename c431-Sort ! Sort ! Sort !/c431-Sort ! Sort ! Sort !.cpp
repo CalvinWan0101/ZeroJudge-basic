f854783e280a38950f1b28eed62428a6a943d131
協同意大利麵的最後希望 <s0610078@my.cmsh.cyc.edu.tw>
2021-02-10 00:21:14 +0800

    Update c431-Sort ! Sort ! Sort !.cpp

--- c431-Sort ! Sort ! Sort !/c431-Sort ! Sort ! Sort !.cpp
+++ c431-Sort ! Sort ! Sort !/c431-Sort ! Sort ! Sort !.cpp
@@ -1,17 +1,22 @@
-#include <stdio.h>
-int n, tmp, list[101];
+#include <stdio.h> 
+using namespace std;
 int main()
 {
+    int n, input, bucket[101] = {0};
     scanf(" %d", &n);
-    while (n--)
+    for (int i = 1; i <= n; i++)
     {
-        scanf(" %d", &tmp);
-        list[tmp]++;
+        scanf(" %d", &input);
+        bucket[input]++;
     }
     for (int i = 1; i <= 100; i++)
     {
-        while (list[i]--)
-            printf("%d ", i);
+        if (bucket[i] != 0)
+            for (int j = 1; j <= bucket[i]; j++)
+            {
+                printf("%d", i);
+                printf(" ");
+            }
     }
     return 0;
 }
