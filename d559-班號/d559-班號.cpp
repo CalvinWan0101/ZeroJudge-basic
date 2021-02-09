6b90ae3e82131dd237bbd55923cb1f493fa106bb
協同意大利麵的最後希望 <s0610078@my.cmsh.cyc.edu.tw>
2021-02-09 23:51:28 +0800

    Update and rename d559-班號.cpp to d559-班號/d559-班號.cpp

---
+++ d559-班號/d559-班號.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int n;
+    while (cin >> n)
+    {
+        cout << "\'C\' can use printf(\"%d\",n); to show integer like " << n << endl;
+    }
+    return 0;
+}
