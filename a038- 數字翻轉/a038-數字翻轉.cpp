3a3708ede89a3e135529cc8d181d172b21f2ce78
意大利麵的最後希望 <s0610078@my.cmsh.cyc.edu.tw>
2021-02-17 17:09:24 +0800

    Update a038-數字翻轉.cpp

--- a038- 數字翻轉/a038-數字翻轉.cpp
+++ a038- 數字翻轉/a038-數字翻轉.cpp
@@ -1,21 +1,23 @@
-#include<iostream>
-#include<string>
+#include <iostream>
+#include <string>
 using namespace std;
 int main()
 {
-	string a;
-	int start;
-	while (cin >> a)
-	{
-		start = 0;
-		for (int i = a.length() - 1; i >= 0; i--)
-			if (a[i] != '0')
-			{
-				start = i;
-				break;
-			}
-		for (int i = start; i >= 0; i--)
-			cout << a[i];
-	}
-	return 0;
+    string a;
+    cin >> a;
+    int k = -1;
+    //找非0記錄k
+    for (int i = a.length() - 1; i >= 0; i--)
+        if (a[i] != '0')
+        {
+            k = i;
+            break;
+        }
+    // k==-1代表整個字串全部都是零，不論1個零還是100個零輸出都是一個零
+    if (k == -1)
+        cout << '0' << endl;
+    else
+        for (int i = k; i >= 0; i--)
+            cout << a[i];
+    return 0;
 }
