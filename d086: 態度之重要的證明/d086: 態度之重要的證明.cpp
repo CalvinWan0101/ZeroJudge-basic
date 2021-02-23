1502eba5952e817bf91430b5752447ca7030708c
意大利麵的最後希望 <s0610078@my.cmsh.cyc.edu.tw>
2021-02-23 13:52:48 +0800

    Update and rename d086-態度之重要的證明.cpp to d086: 態度之重要的證明/d086: 態度之重要的證明.cpp

---
+++ d086: 態度之重要的證明/d086: 態度之重要的證明.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    char Balp[27] = {'0', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+    char Salp[27] = {'0', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+    string a;
+    int count;
+    int buffer;
+    int error;
+    while (cin >> a)
+    {
+        error = 0;
+        if (a[0] == '0' && a.length() == 1)
+            break;
+        count = 0;
+        for (int i = 0; i < a.length(); i++)
+        {
+            buffer = count;
+            for (int j = 1; j <= 26; j++)
+                if (a[i] == Balp[j])
+                    count += j;
+            for (int j = 1; j <= 26; j++)
+                if (a[i] == Salp[j])
+                    count += j;
+            if (count == buffer)
+                error = 1;
+        }
+        if (error == 0)
+            cout << count << endl;
+        else
+            cout << "Fail" << endl;
+    }
+    return 0;
+}
