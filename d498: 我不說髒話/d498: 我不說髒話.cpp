20f4f73b74e33803fb96cb32ce2ed19bb2180e2d
意大利麵的最後希望 <s0610078@my.cmsh.cyc.edu.tw>
2021-02-23 12:51:02 +0800

    Update and rename d498-我不說髒話.cpp to d498: 我不說髒話/d498: 我不說髒話.cpp

---
+++ d498: 我不說髒話/d498: 我不說髒話.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int n;
+    while (cin >> n)
+        for (int i = 1; i <= n; i++)
+            cout << "I don't say swear words!" << endl;
+    return 0;
+}
