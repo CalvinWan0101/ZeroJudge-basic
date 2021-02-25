e97bb44aefdb558424ce2bc02f59871e9ba0135d
意大利麵的最後希望 <s0610078@my.cmsh.cyc.edu.tw>
2021-02-25 19:01:16 +0800

    Update and rename d073-分組報告.cpp to d073: 分組報告/d073: 分組報告.cpp

---
+++ d073: 分組報告/d073: 分組報告.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int n;
+    while (cin >> n)
+        cout << (n + 2) / 3 << endl;
+    return 0;
+}
